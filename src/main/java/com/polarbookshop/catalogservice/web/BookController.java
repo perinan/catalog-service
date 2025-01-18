6c646f5b9db46ef1875ec945ec7dfe696b7756e1
José Antonio Periñán <jose-antonio.perinan@ext.leroymerlin.es>
2025-01-18 20:49:42 +0100

    add tilt config and validate kubernetes manifest

--- src/main/java/com/polarbookshop/catalogservice/web/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookController.java
@@ -4,22 +4,28 @@ import com.polarbookshop.catalogservice.domain.Book;
 import com.polarbookshop.catalogservice.domain.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
-@RequestMapping("books")
+@RequestMapping("/books")
 @RequiredArgsConstructor
 class BookController {
     private final BookService bookService;
 
     @GetMapping
     Iterable<Book> get () {
+        log.info("Retrieving all books");
+
         return bookService.viewBookList();
     }
 
     @GetMapping("{isbn}")
     Book getByIsbn(@PathVariable String isbn) {
+        log.info("Retrieving book with ISBN: {}", isbn);
+
         return bookService.viewBookDetails(isbn);
     }
 
@@ -37,6 +43,8 @@ class BookController {
 
     @PutMapping("{isbn}")
     Book put(@PathVariable String isbn, @Valid @RequestBody Book book) {
+        log.info("Updating book with ISBN: {}", isbn);
+
         return bookService.editBookDetails(isbn, book);
     }
 }
